96b75db6e3f7e1f0bd5ab7453819be103d21e898
magvtv <magutujr@gmail.com>
2023-11-08 17:26:41 +0300

    functional nav menu on mobile render

--- assets/js/academy.js
+++ assets/js/academy.js
@@ -1,7 +1,15 @@
-funciton validateForm() {
-    let f = document.forms["form"]["regno"].value;
-    if (f == "") {
-        alert("Please insert your registration number");
-        return false;
-    }
+const navMenu = document.getElementById('nav-menu'),
+	navToggle = document.getElementById('nav-toggle'),
+	navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show__menu')
+    })
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show__menu')
+    })
 }
