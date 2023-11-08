60e4beac439a7043b8d61b9215e88c414923ddaa
magvtv <magutujr@gmail.com>
2023-11-08 18:03:37 +0300

    things to be done for KES 5000, come to this early morning

--- assets/js/academy.js
+++ assets/js/academy.js
@@ -1,15 +1,24 @@
+// show menu on mobile screen
 const navMenu = document.getElementById('nav-menu'),
 	navToggle = document.getElementById('nav-toggle'),
 	navClose = document.getElementById('nav-close');
 
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show__menu')
-    })
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show__menu');
+	});
 }
 
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show__menu')
-    })
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show__menu');
+	});
 }
+
+// remove mobile menu after making choice
+const navLink = document.querySelectorAll('.nav__link');
+
+function removeLink() {
+    navMenu.classList.remove('show__menu')
+}
+navLink.forEach(link => link.addEventListener('click', removeLink))
